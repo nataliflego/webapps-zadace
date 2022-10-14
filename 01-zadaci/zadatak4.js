bc869b0cc292768295f12325fbd9ed0ffc2d9e0d
NataliFlego <natali.flego6@gmail.com>
2022-10-14 09:47:26 +0200

    Rjesen zadatak 4

---
+++ 01-zadaci/zadatak4.js
@@ -0,0 +1,12 @@
+//Napiši funkciju koja pretvara broj X u sate i minute
+//Ispis : 120 -> “Ovo je 2 sata i 0 minuta”
+
+var satiminute = function (x) {
+    var sat = Math.floor(x / 60)
+    var min = x % 60
+    console.log("Ovo je ", sat, " sata i ", min, " minuta")
+}
+
+satiminute(120)
+satiminute(140)
+satiminute(450)
